7e580b99259529bc47ff74f61276501bbf4a28d0
nilton <ngcquimica@gmail.com>
2025-11-30 08:08:55 -0300

    feat: IRepository pattern implemented

---
+++ VShop.ProductApi/Repositories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+using VShop.ProductApi.Models;
+
+namespace VShop.ProductApi.Repositories;
+
+public interface ICategoryRepository
+{
+    Task<IEnumerable<Category>> GetAll();
+    Task<IEnumerable<Category>> GetCategoriesProducts();
+    Task<Category> GetById(int id);
+    Task<Category> Create(Category category);
+    Task<Category> Update(Category category);
+    Task<Category> Delete(int id);
+}
